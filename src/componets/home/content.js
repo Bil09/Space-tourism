4089fedd55325cf69c1a6b401a5d92b56aa3b82a
Bilhid <bil.bilhid09@gmail.com>
2022-03-24 22:30:28 +0100

    styled home

--- src/componets/home/content.js
+++ src/componets/home/content.js
@@ -3,13 +3,13 @@ import '../../styles/content.css';
 const Content = () => {
    return(
       <div className="content">
-         <h5>So yo want to travel to</h5>
-         <h1>Space</h1>
-         <p>
-            Lets face it; if you want yo go to space,you might<br />
-            as well genuinely go to outer space and not hover<br />
-            kind of on the edge of it. Well sit back, and relax<br />
-            because we'll give you a truly out of this world<br />
+         <h5 className='tx-center'>So yo want to travel to</h5>
+         <h1 className='tx-center'>SPACE</h1>
+         <p className='tx-center'>
+            Lets face it; if you want yo go to space,you might
+            as well genuinely go to outer space and not hover
+            kind of on the edge of it. Well sit back, and relax
+            because we'll give you a truly out of this world
             experience!
          </p>
       </div>
